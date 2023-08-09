291684d3e1c4466956f162db3f9db5e964b90d62
fujun <14149812@qq.com>
2023-08-09 22:03:35 +0800

    Initial commit

---
+++ src/tmp/JQName.h
@@ -0,0 +1,30 @@
+
+#ifndef CAL_JQNAME_H
+#define CAL_JQNAME_H
+
+const int VERNAL_EQUINOX      = 0;    // 春分
+const int CLEAR_AND_BRIGHT    = 1;    // 清明
+const int GRAIN_RAIN          = 2;    // 谷雨
+const int SUMMER_BEGINS       = 3;    // 立夏
+const int GRAIN_BUDS          = 4;    // 小满
+const int GRAIN_IN_EAR        = 5;    // 芒种
+const int SUMMER_SOLSTICE     = 6;    // 夏至
+const int SLIGHT_HEAT         = 7;    // 小暑
+const int GREAT_HEAT          = 8;    // 大暑
+const int AUTUMN_BEGINS       = 9;    // 立秋
+const int STOPPING_THE_HEAT   = 10;   // 处暑
+const int WHITE_DEWS          = 11;   // 白露
+const int AUTUMN_EQUINOX      = 12;   // 秋分
+const int COLD_DEWS           = 13;   // 寒露
+const int HOAR_FROST_FALLS    = 14;   // 霜降
+const int WINTER_BEGINS       = 15;   // 立冬
+const int LIGHT_SNOW          = 16;   // 小雪
+const int HEAVY_SNOW          = 17;   // 大雪
+const int WINTER_SOLSTICE     = 18;   // 冬至
+const int SLIGHT_COLD         = 19;   // 小寒
+const int GREAT_COLD          = 20;   // 大寒
+const int SPRING_BEGINS       = 21;   // 立春
+const int THE_RAINS           = 22;   // 雨水
+const int INSECTS_AWAKEN      = 23;   // 惊蛰
+
+#endif //CAL_JQNAME_H
